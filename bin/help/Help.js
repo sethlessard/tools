6e1921598080b07b13ec4efaba0be483614b2a13
Seth Lessard <sethlessard@outlook.com>
2020-06-28 17:37:58 +0000

    Fix issues with spacing

--- bin/help/Help.js
+++ bin/help/Help.js
@@ -24,17 +24,32 @@ const showHelp = (subcommand) => {
     hmsg += "    [language]:\n";
     for (const language of Object.keys(scHelp.languages)) {
       const langInfo = scHelp.languages[language];
-      hmsg += `        - ${language}                        ${langInfo.description}\n`
+      hmsg += _formatSubjectDescription(`        - ${language}`, langInfo.description) + "\n";
       hmsg += `            [type]\n`
       for (const type of Object.keys(langInfo.help)) {
         const typeHelp = langInfo.help[type];
-        hmsg += `                * ${type}                        ${typeHelp.description}\n`;
+        hmsg += _formatSubjectDescription(`                * ${type}`, typeHelp.description, 64) + "\n";
       }
     }
     hmsg += "\n";
   }
 
   _showUsage(hmsg);
+};
+
+/**
+ * Format a subject/description so that the spacing is even for all entries.
+ * @param {string} subject the subject.
+ * @param {string} description the description of the subject.
+ * @param {number} descriptionStart the start of the description field.
+ * @returns {string} the formatted subject/description line
+ */
+const _formatSubjectDescription = (subject, description, descriptionStart = 60) => {
+  const subjectLength = subject.length;
+  for (let i = subject.length; i < descriptionStart; i++) {
+    subject += " ";
+  }
+  return subject + description;
 };
 
 /**
