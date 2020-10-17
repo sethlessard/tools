e7ef8c0cc76bc0e2025124f2e3c4777bf2faa1fa
Seth Lessard <sethlessard@outlook.com>
2020-10-17 15:30:15 -0500

    More progress on newProject

--- src/util/WindowUtils.ts
+++ src/util/WindowUtils.ts
@@ -7,6 +7,16 @@ export type YesNoOptions = {
 };
 
 /**
+ * Prompt for input and require that something was input.
+ * @param prompt the prompt.
+ * @param placeHolder the placeholder to show.
+ * @param value the default value.
+ */
+const promptInputRequireValue = (prompt: string, placeHolder?: string, value?: string, ) => {
+  return window.showInputBox({ prompt, placeHolder, value, validateInput: (value?: string) => (value) ? null : "You must enter a value" });
+};
+
+/**
  * Prompt a Yes/No Question to the user.
  * @param options the options.
  */
@@ -17,5 +27,6 @@ const promptYesNo = (options: YesNoOptions) => {
 };
 
 export {
+  promptInputRequireValue,
   promptYesNo
 };
