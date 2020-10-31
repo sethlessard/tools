f7de8ea788699a63f1d4da7fddc2ff2448313bcc
Seth Lessard <sethlessard@outlook.com>
2020-10-30 23:06:59 -0500

    Convert Additional setup to bash script

--- .azure-pipelines/AdditionalSetup.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Starting Xvfb.."
-Start-Job -ScriptBlock {
-  /usr/bin/Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 
-} -ErrorAction Stop
-
-Start-Sleep -Seconds 5
-
-Write-Host "Started Xvfb.."
